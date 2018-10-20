aedba5f17f63676fbaa38b0bdc34c67dc7c7e4f8
Yuraist <yura.ist@icloud.com>
2018-10-20 13:25:27 +0300

    Implemented a database layer

---
+++ lib/persistence/dblayer/dblayer.go
@@ -0,0 +1,21 @@
+package dblayer
+
+import (
+	"chill_wave/lib/persistence"
+	"chill_wave/lib/persistence/mongolayer"
+)
+
+type DBTYPE string
+
+const (
+	MONGODB DBTYPE = "mongodb"
+	DYNAMODB DBTYPE = "dynamodb"
+)
+
+func NewPersistenceLayer(options DBTYPE, connection string) (persistence.DatabaseHandler, error) {
+	switch options {
+	case MONGODB:
+		return mongolayer.NewMongoDBLayer(connection)
+	}
+	return nil, nil
+}
