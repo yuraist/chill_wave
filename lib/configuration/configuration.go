6fba3c72224059b9fc0b553b5593d59d56c5809a
Yuraist <yura.ist@icloud.com>
2018-10-20 13:21:53 +0300

    Implemented a configuration layer of the microservice

---
+++ lib/configuration/configuration.go
@@ -0,0 +1,37 @@
+package configuration
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+var (
+	DBTypeDefault          = dblayer.DBTYPE("mongodb")
+	DBConnectionDefault    = "mongodb:127.0.0.1"
+	RestfulEndpointDefault = "localhost:8181"
+)
+
+type ServiceConfig struct {
+	DatabaseType    dblayer.DBTYPE `json:"database_type"`
+	DBConnection    string         `json:"db_connection"`
+	RestfulEndpoint string         `json:"resfulapi_endpoint"`
+}
+
+func ExtractConfiguration(filename string) (ServiceConfig, error) {
+	conf := ServiceConfig{
+		DBTypeDefault,
+		DBConnectionDefault,
+		RestfulEndpointDefault,
+	}
+
+	file, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Println("Configuration file not found. Continuing with default values.")
+		return conf, err
+	}
+
+	err = json.NewDecoder(file).Decode(&conf)
+	return conf, err
+}
